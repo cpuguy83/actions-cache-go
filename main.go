45858b5767a590e130af999c20ebae0eba333eae
Brian Goff <cpuguy83@gmail.com>
2025-03-12 12:34:38 -0700

    Add singleflight to get

--- main.go
+++ main.go
@@ -89,7 +89,7 @@ func do(ctx context.Context, cacheDirPath string, in io.Reader, out io.Writer) e
 
 	var flight singleflight.Group
 	srv := &gocache.Server{
-		Get: handleGet(client, cacheDir),
+		Get: handleGet(client, cacheDir, &flight),
 		Put: handlePut(client, cacheDir, &wg, &flight),
 		Close: func(context.Context) error {
 			wg.Wait()
@@ -105,37 +105,50 @@ type getHandlerFunc func(ctx context.Context, actionID string) (outputID, diskPa
 
 type putHandlerFunc func(ctx context.Context, req gocache.Object) (diskPath string, _ error)
 
-func handleGet(client *actionscache.Cache, local *cachedir.Dir) getHandlerFunc {
+func handleGet(client *actionscache.Cache, local *cachedir.Dir, flight *singleflight.Group) getHandlerFunc {
+	type ret struct {
+		outputID, diskPath string
+	}
+
 	return func(ctx context.Context, actionID string) (outputID, diskPath string, _ error) {
-		id, path, err := local.Get(ctx, actionID)
-		if err != nil {
+		v, err, _ := flight.Do(actionID, func() (interface{}, error) {
+			id, path, err := local.Get(ctx, actionID)
+			if err != nil {
+				return nil, err
+			}
+			if id != "" {
+				return &ret{id, path}, nil
+			}
+
+			entry, err := client.Load(ctx, actionID)
+			if err != nil {
+				return nil, fmt.Errorf("error loading cache key %q: %w", actionID, err)
+			}
+			if entry == nil {
+				return nil, nil
+			}
+
+			remote := entry.Download(ctx)
+			defer remote.Close()
+
+			obj := gocache.Object{
+				ActionID: actionID,
+				Body:     io.NewSectionReader(remote, 0, math.MaxInt64),
+				OutputID: actionID,
+			}
+			p, err := local.Put(ctx, obj)
+			if err != nil {
+				return nil, fmt.Errorf("error storing in local cache: %w", err)
+			}
+			return &ret{id, p}, nil
+		})
+
+		if err != nil || v == nil {
 			return "", "", err
 		}
-		if id != "" {
-			return id, path, nil
-		}
 
-		entry, err := client.Load(ctx, actionID)
-		if err != nil {
-			return "", "", fmt.Errorf("error loading cache key %q: %w", actionID, err)
-		}
-		if entry == nil {
-			return "", "", nil
-		}
-
-		remote := entry.Download(ctx)
-		defer remote.Close()
-
-		obj := gocache.Object{
-			ActionID: actionID,
-			Body:     io.NewSectionReader(remote, 0, math.MaxInt64),
-			OutputID: actionID,
-		}
-		p, err := local.Put(ctx, obj)
-		if err != nil {
-			return "", "", fmt.Errorf("error storing in local cache: %w", err)
-		}
-		return "", p, nil
+		vv := v.(*ret)
+		return vv.outputID, vv.diskPath, nil
 	}
 }
 
@@ -169,7 +182,6 @@ func handlePut(client *actionscache.Cache, local *cachedir.Dir, wg *sync.WaitGro
 
 					var attrs []slog.Attr
 					attrs = append(attrs, slog.String("actionID", req.ActionID))
-					attrs = append(attrs, slog.String("message", err.Error()))
 					if errors.As(err, &he) {
 						if he.StatusCode == http.StatusConflict {
 							// Cache already exists
@@ -177,7 +189,7 @@ func handlePut(client *actionscache.Cache, local *cachedir.Dir, wg *sync.WaitGro
 						}
 						attrs = append(attrs, slog.Int("statusCode", he.StatusCode))
 					}
-					slog.LogAttrs(ctx, slog.LevelError, "error saving to github actions cache", attrs...)
+					slog.LogAttrs(ctx, slog.LevelError, err.Error(), attrs...)
 				}
 				return nil, nil
 			})
